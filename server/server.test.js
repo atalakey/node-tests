5935bdd5f5670aa9e9b6cd494fe427024bdc776e
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-08-25 22:27:45 +0300

    organize tests using the describe() function

--- server/server.test.js
+++ server/server.test.js
@@ -3,32 +3,40 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(200)
-    .expect('Hello world!')
-    .end(done);
-});
+describe('Server', () => {
+  describe('GET /', () => {
+    it('should return hello world response', (done) => {
+      request(app)
+        .get('/')
+        .expect(200)
+        .expect('Hello world!')
+        .end(done);
+    });
+  });
 
-it('should return JSON response with an error and name property', (done) => {
-  request(app)
-    .get('/json')
-    .expect(404)
-    .expect({
-      error: 'Page not found',
-      name: 'Todo App v1.0'
-    })
-    .end(done);
-});
+  describe('GET /json', () => {
+    it('should return JSON response with an error and name property', (done) => {
+      request(app)
+        .get('/json')
+        .expect(404)
+        .expect({
+          error: 'Page not found',
+          name: 'Todo App v1.0'
+        })
+        .end(done);
+    });
+  });
 
-// using the expect assertion library
-it('should return my user object', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({ name: 'John', age: 20 });
-    })
-    .end(done);
+  describe('GET /users', () => {
+    // using the expect assertion library
+    it('should return my user object', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({ name: 'John', age: 20 });
+        })
+        .end(done);
+    });
+  });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,47 +2,61 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var res = utils.add(33, 11);
+describe('Utils', () => {
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      var res = utils.add(33, 11);
+    
+      expect(res).toBe(44).toBeA('number');
+    });
+  });
 
-  expect(res).toBe(44).toBeA('number');
-});
+  describe('#asyncAdd', () => {
+    it('should async add two numbers', (done) => {
+      utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+      });
+    });
+  });
 
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+  describe('#square', () => {
+    it('should square a number', () => {
+      var res = utils.square(3);
+    
+      expect(res).toBe(9).toBeA('number');
+    });
+  });
+
+  describe('#asyncSquare', () => {
+    it('should async square a number', (done) => {
+      utils.asyncSquare(5, (res) => {
+        expect(res).toBe(25).toBeA('number');
+        done();
+      });
+    });
+  });
+
+  describe('#setName', () => {
+    it('should set firstName and lastName', () => {
+      var user = { age: 20, location: 'some location' };
+      var res = utils.setName(user, 'John Doe');
+    
+      // objects in JavaScript are passed by reference
+      expect(user).toEqual(res);
+      expect(res).toInclude({ firstName: 'John', lastName: 'Doe' }).toBeA('object');
+    });
   });
 });
 
-it('should square a number', () => {
-  var res = utils.square(3);
-
-  expect(res).toBe(9).toBeA('number');
-});
-
-it('should async square a number', (done) => {
-  utils.asyncSquare(5, (res) => {
-    expect(res).toBe(25).toBeA('number');
-    done();
+describe('other', () => {
+  it('should expect some values', () => {
+    expect(12).toNotBe(11);
+    expect({name: 'John'}).toEqual({name: 'John'});
+    expect({name: 'John'}).toNotEqual({name: 'john'});
+    expect([2,3,4]).toInclude(2);
+    expect([2,3,4]).toExclude(1);
+    expect({ name: 'John', age: 20, location: 'some location' }).toInclude({ age: 20 });
+    expect({ name: 'John', age: 20, location: 'some location' }).toExclude({ age: 23 });
   });
-});
-
-it('should set firstName and lastName', () => {
-  var user = { age: 20, location: 'some location' };
-  var res = utils.setName(user, 'John Doe');
-
-  // objects in JavaScript are passed by reference
-  expect(user).toEqual(res);
-  expect(res).toInclude({ firstName: 'John', lastName: 'Doe' }).toBeA('object');
-});
-
-it('should expect some values', () => {
-  expect(12).toNotBe(11);
-  expect({name: 'John'}).toEqual({name: 'John'});
-  expect({name: 'John'}).toNotEqual({name: 'john'});
-  expect([2,3,4]).toInclude(2);
-  expect([2,3,4]).toExclude(1);
-  expect({ name: 'John', age: 20, location: 'some location' }).toInclude({ age: 20 });
-  expect({ name: 'John', age: 20, location: 'some location' }).toExclude({ age: 23 });
 });
